4a3abb865a482ad9661bdbe187c5b049d1fa6e71
Shakxtra - Milsim <50801784+shakxtra@users.noreply.github.com>
2026-02-16 20:47:17 -0600

    Add files via upload

---
+++ @160th SOAR/meta.cpp
@@ -0,0 +1,4 @@
+protocol = 1;
+publishedid = 0;
+name = "16TH SOAR by Shakxtra-Milsim";
+timestamp = 2036458791004123284;
---
+++ @160th SOAR/mod.cpp
@@ -0,0 +1,19 @@
+// Nombre del mod
+name = "160th SOAR";
+// Tool tip mostrado al pasar el mouse, en el menu principal.
+tooltipOwned= "Shakxtra - Milsim";
+// Imagen mostrada en el menu de expansiones / la resolusion optima es 2048x1024 pixeles
+picture = "160th.paa";
+// Imagen mostrada en los item agregados en el mod en arsenal virtual
+logoSmall = "160thsmall.paa";
+// Logotipo mostrado en el menu principal
+logo = "160th.paa";
+// Cuando el mouse no esta activo, en el menu principal.
+logoOver = "160th.paa";
+// Oculta nombre de extension en el menu principal
+hidePicture = 0;
+hideName = 0;
+actionName = "Website";
+action = "https://www.youtube.com/shakxtra";
+description = "160th SOAR Official Mod";
+overview="Shakxtra - Milsim";
